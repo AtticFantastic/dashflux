a97e3a95eb394052f2e86ab201b5828c7d74d1e3
Drasko DRASKOVIC <drasko.draskovic@gmail.com>
2016-09-11 00:43:34 +0200

    Add Material 2 components
    Signed-off-by: Drasko DRASKOVIC <drasko.draskovic@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+	template: `
+		<button md-raised-button color=’primary’>this is a magical button</button>
+	`
 })
 export class AppComponent {
   title = 'app works!';
